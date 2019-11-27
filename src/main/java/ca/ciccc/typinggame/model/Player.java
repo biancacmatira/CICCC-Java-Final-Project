4cf09c0d1b098740fbd07342cf169c49b09bf6fc
Bianca <biancacmatira@gmail.com>
2019-11-26 19:42:09 -0800

    Changed Player fields to private

--- src/main/java/ca/ciccc/typinggame/model/Player.java
+++ src/main/java/ca/ciccc/typinggame/model/Player.java
@@ -3,6 +3,22 @@ package ca.ciccc.typinggame.model;
 import java.util.UUID;
 
 public class Player {
-  public String name;
-  public UUID id;
+  private String name;
+  private UUID id;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
 }
