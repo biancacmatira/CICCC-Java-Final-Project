d49ef8d63ecc7390f5823774724a269f6ef985c2
YukiSugaya <sugaya324@gmail.com>
2019-11-26 16:40:29 -0800

    Modify WordGenerator class

--- src/main/java/ca/ciccc/typinggame/model/WordGenerator.java
+++ src/main/java/ca/ciccc/typinggame/model/WordGenerator.java
@@ -3,7 +3,16 @@ package ca.ciccc.typinggame.model;
 public class WordGenerator {
   private String[] wordList;
 
+  public WordGenerator(String[] wordList) {
+    this.wordList = wordList;
+  }
+
   public Word generateWord(int length) {
+    /**
+     * TODO: read text file to load words list. add each word to wordList.
+     *
+     * @param length length of each word
+     */
     return null;
   }
 }
